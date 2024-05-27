3dddfa12c41615d380bec898f9938a178c816ac9
Mohammad Kareem <126200282+codehis@users.noreply.github.com>
2024-05-27 17:17:38 +0000

    variables

--- 01_basic.js
+++ 01_basic.js
@@ -1,1 +1,40 @@
-console.log("mohammad kareem")
+// var , let and const
+
+
+
+// var , let , const :- 
+
+// when js developed by breaden eich that time every one used var for variable declearation by due to some drawback 
+
+// List of drawback  :- 1) var is scope free it means parent class can acess the variable data of child class
+
+//   example :- 
+
+// function car(){
+     
+//     for(var i=0; i<6;i++){
+//         console.log(" this is local scope ", i)
+//     }
+//     console.log("this is parent scope ",i)
+//  }
+
+//  car()
+
+// 2) whenever you declear any variable through var type the automatic variable add in window 
+// object it is security issue when we build web site example
+
+// let -:  let  is a new keyword of update js enviroment 
+
+// const -: const once you declear then it is not changed untill the program ended
+
+
+// console.table([])
+
+// const name ="mohammad kareem";
+//  const universuty="kanpur university"
+//  const course ='Bca '
+//  let fees ='paid';
+//  let placementStatus='not selected '
+
+
+//  console.table([name,universuty,course,fees,placementStatus])
