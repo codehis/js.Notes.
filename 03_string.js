3700cd9b3345fb9355cd7d5ee69a6a203b3b13a1
Mohammad Kareem <126200282+codehis@users.noreply.github.com>
2024-05-29 05:17:00 +0000

    string

---
+++ 03_string.js
@@ -0,0 +1,48 @@
+// String 
+
+// here we can declear the string by two way 
+// one is 
+let name="john"
+let secondName='papa'
+
+// second way 
+let FirstName =new String('mummy')
+
+// console.log(name[0])
+// console.log(FirstName[1])
+
+
+// function in string 
+/*  chatAt :- we can find the charector at what index by putting the index number then we get 
+character at the index
+
+console.log(FirstName.charAt(1))
+*/
+
+// comparing string
+/*toUpperCase and toLowerCase
+
+console.log(FirstName.toLowerCase())
+ console.log(FirstName.toUpperCase())
+ */
+
+ /*concate:- once you create a string then we have option to add some 
+                thing new information in 
+         your variable like in below we mention
+
+console.log(FirstName.concat(' have good behavior'))
+ */
+
+/* trim() :- is the method where remove the white spaces from the string
+console.log(FirstName.trim())*/
+
+
+/* repeat() :- the sting print for twice according to repeat fucntion
+console.log(Firstname.repeat(2));
+
+*/
+
+/*replace("first word","second word"):- replce the word form the string
+
+console.log(FirstName.replace('mummy','kareem'))
+*/
